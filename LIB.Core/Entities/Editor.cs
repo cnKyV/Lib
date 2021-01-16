aae6ab500e6cb476e5ebf315e3f9cdbb92a24fcc
cnKyV <cenkay_vergili4@hotmail.com>
2021-01-16 20:14:35 +0300

    ss

---
+++ LIB.Core/Entities/Editor.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LIB.Core.Entities
+{
+    public class Editor
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string About { get; set; }
+        public DateTime? DateOfBirth { get; set; }
+        public ICollection<Book> Books { get; set; }
+        public ICollection<Publisher> Publishers { get; set; }
+        public Contact Address { get; set; }
+    }
+}
